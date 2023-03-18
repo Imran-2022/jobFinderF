b22ed3526f723b34a53d1bcdd9072689533f00e1
Imran-2022 <mdimranulhaque.202@gmail.com>
2023-03-19 02:51:05 +0600

    connect backend server

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001",
+    // baseURL: "http://localhost:3001",
+    baseURL: import.meta.env.VITE_APP_PORT,
 });
 
 export default axiosInstance;
